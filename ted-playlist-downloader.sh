309ce779d7df0be2b2d98d5d18a143339bc609c7
shivams <shivam.src@gmail.com>
2015-09-13 22:16:05 +0530

    Added the capability to extract the playlist title and then download all the videos into a directory based on that playlist's name

--- ted-playlist-downloader.sh
+++ ted-playlist-downloader.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#First extract the playlist title and then create a directory based on that name and cd into it
+playlist_title=`curl "$1" | hxnormalize -xe | hxselect -c title | grep -oP ".+(?=\|\ Playlist.+)"`
+mkdir "$playlist_title"
+cd "$playlist_title"
+
+#Now, extract all the video URLs from the playlist and download them in a loop using youtube-dl 
 curl "$1" | hxnormalize -xe | hxselect h9 a.playlist-talks__play | grep -oP 'href="\K.+?(?=")' | while read -r line; 
 do 
 	link="https://www.ted.com""$line"; 
